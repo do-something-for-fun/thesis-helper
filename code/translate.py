43286d7de2f1ac8d3153cf3aed1007425ecb6111
Mu Hualing <hualing.mu@outlook.com>
2019-11-06 08:41:12 +0800

    增加google翻译，并修改部分变量名

--- code/translate.py
+++ code/translate.py
@@ -5,6 +5,17 @@ subscription_key = '32f1cb9c935a4cd4b33825e2869bff0f'
 
 
 def get_translation_by_google(text_input):
+    # 去除pdf格式所造成的换行问题
+    text_input = text_input.split("\n")
+    text = ""
+    new_begin = True
+    length = len(text_input[0]) - 10
+    for line in text_input:
+        last_line = len(line) < length
+        text += ((not new_begin) * " " + line.rstrip("-") + "\n" * last_line)
+        new_begin = last_line or line[-1] == "-"
+    text_input = text
+
     translator = Translator(service_urls=["translate.google.cn"])
     translate_res = translator.translate(text_input, dest='zh-cn')
     return translate_res.text

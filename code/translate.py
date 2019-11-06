d1b91f62f644e62850411e2b0a39612726a03471
lujiawei <505509679@qq.com>
2019-11-06 20:15:08 +0800

    修改了googletrans的bug

--- code/translate.py
+++ code/translate.py
@@ -1,8 +1,28 @@
 import os, requests, uuid, json
 from googletrans import Translator
-
+from googletrans import urls, utils
+from googletrans.compat import PY3
+from googletrans.constants import DEFAULT_USER_AGENT
 subscription_key = '32f1cb9c935a4cd4b33825e2869bff0f'
 
+class MyTranslator(Translator):
+    def __init__(self,service_urls=None, user_agent=DEFAULT_USER_AGENT,
+                 proxies=None, timeout=None):
+        super().__init__(service_urls,user_agent,proxies,timeout)
+
+    def _translate(self, text, dest, src):
+        if not PY3 and isinstance(text, str):  # pragma: nocover
+            text = text.decode('utf-8')
+
+        token = self.token_acquirer.do(text)
+        params = utils.build_params(query=text, src=src, dest=dest,
+                                    token=token)
+        params['client'] = 'webapp'
+        url = urls.TRANSLATE.format(host=self._pick_service_url())
+        r = self.session.get(url, params=params)
+
+        data = utils.format_json(r.text)
+        return data
 
 def get_translation_by_google(text_input):
     # # 去除pdf格式所造成的换行问题
@@ -16,7 +36,7 @@ def get_translation_by_google(text_input):
     #     new_begin = last_line or line[-1] == "-"
     # text_input = text
 
-    translator = Translator(service_urls=["translate.google.cn"])
+    translator = MyTranslator(service_urls=["translate.google.cn"])
     translate_res = translator.translate(text_input, dest='zh-cn')
     return translate_res.text
 
